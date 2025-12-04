42a6d9411dc68465787bb33e2412f7d56861cabc
Bhavya17032003 <bhavyakanchukommala@gmail.com>
2025-12-05 01:16:56 +0530

    Delete Virtual_Assistant_With_Banking_&_Email_Services directory

--- Virtual_Assistant_With_Banking_&_Email_Services/email_modules/bulk_email_sender.py
+++
@@ -1,8 +0,0 @@
-from email_modules.single_email_sender import singleEmailSender
-
-def bulkEmailsender(list_of_emails, subject, body):
-    for email in list_of_emails:
-        try:
-            singleEmailSender(to_email=email, subject=subject, body=body)
-        except Exception as e:
-            print(f"Failed to send email to {email}. Error: {e}")
